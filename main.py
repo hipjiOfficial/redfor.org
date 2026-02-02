a4cf2d680fc8b6dfb2c11f1399a91f63b5df5fee
hipji <115195044+hipjiOfficial@users.noreply.github.com>
2026-02-01 17:42:38 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+import psycopg2
+import os
+from datetime import datetime, timezone
+from pathlib import Path
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
+connection_string = os.getenv("DB_CONNECTION_STRING")
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/shop")
+def get_shop():
+    conn = psycopg2.connect(connection_string)
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT pc1, pc2, pc3, log_date
+            FROM shop_log
+            WHERE log_date <= %s
+            ORDER BY log_date DESC
+            LIMIT 1
+        """, (datetime.now(timezone.utc),))
+
+        row = cur.fetchone()
+
+    conn.close()
+
+    if not row:
+        return {"error": "No shop log found"}
+    
+    return {
+        "pc1": row[0],
+        "pc2": row[1],
+        "pc3": row[2],
+        "log_date": row[3].isoformat()
+    }
