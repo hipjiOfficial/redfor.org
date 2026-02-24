9feeb9b7d7e4b71bdb6a292bda2e64cfda668a65
hipji <115195044+hipjiOfficial@users.noreply.github.com>
2026-02-24 15:06:51 -0700

    vFix

--- assets/js/version.js
+++ assets/js/version.js
@@ -8,4 +8,5 @@ document.addEventListener("DOMContentLoaded", () => {
 // window.version = "1.0.0"; // first release
 // window.version = "1.0.1"; // added defuse the bomb gif
 // window.version = "1.1.0"; // gallery page added
-window.version = "1.2.0"; // Javascript rearrangement. Removed every instance of a <script> tag and replaced them with specific .js files. Minor changes to gallery and shop, including rewriting styles to better fit mobile and smaller screens.
+// window.version = "1.2.0"; // Javascript rearrangement. Removed every instance of a <script> tag and replaced them with specific .js files. Minor changes to gallery and shop, including rewriting styles to better fit mobile and smaller screens.
+window.version = "1.2.1"; //  New Site Logo' -m 'Added a temporary favicon.
