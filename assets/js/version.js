b6d9793dffcd55607e3ec133abe9201eb3b9e9d0
hipji <iamhipji@gmail.com>
2026-04-02 21:59:55 -0700

    [1.4.8]
    Added you are an idiot

--- assets/js/version.js
+++ assets/js/version.js
@@ -34,4 +34,5 @@ document.addEventListener("DOMContentLoaded", () => {
 // window.version = "1.4.4"; // Added Cold Front.
 // window.version = "1.4.5"; //updated splatter
 // window.version = "1.4.6"; // Changed the way og-image renders images, now uses imagecreatefromstring instead of imagecreatefrompng, so it doesn't fail if a file isn't a png. I currently have no idea why, but recently the whole page crashed due to this problem. Every playercard asset is a .png, so I have no clue as to what about Dansen.png caused this issue specifically. Started work on a new homepage.
-window.version = "1.4.7"; // Added support page, as well as adding support buttons on specific pages. 
+// window.version = "1.4.7"; // Added support page, as well as adding support buttons on specific pages. 
+window.version = "1.4.8"; // Added You Are An Idiot gif
