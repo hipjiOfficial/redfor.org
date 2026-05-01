8a1705df585c7e5a194f2ad6a4048d1158947c61
hipji <iamhipji@gmail.com>
2026-05-01 09:31:20 -0700

    [1.4.9]
    removed down overlay. currently working on a new system to avoid problems in the future.

--- shop.php
+++ shop.php
@@ -38,14 +38,6 @@
     </div>
     <h4 class="titlepath" id="typed-text" data-text="agent@framework:/usr/home/dailyshop"></h4>
   </header>
-    <div id="down-overlay">
-        <div id="down-box">
-        <h2>Daily Shop is currently down.</h2>
-        <p>I ran out of CU-hrs only 18 days into the month, so the database that the api uses is unavailable.</p>
-	<p>I am working on a fix, but I don't know when it will be ready. Sorry.</p>
-	<p>In the meantime, check out the <a href="https://www.redfor.org/gallery">gallery</a>.</p>
-        </div>
-    </div>
     <div class="shop">
         <div class="shop-cards">
             <img id="pc1">
